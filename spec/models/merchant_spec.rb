9c004e6fd56fab73ba391b1fdd29e27adb23a3ac
Eliot Swank <eliotswank@gmail.com>
2018-06-26 17:27:08 -0600

    add validation for merchant model GREEN

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe Merchant do
   describe "Validations" do
     it "should have a name" do
-      merchant = Merchant.new
+      merchant = Merchant.new(name: nil)
 
       expect(merchant).to_not be_valid
     end
