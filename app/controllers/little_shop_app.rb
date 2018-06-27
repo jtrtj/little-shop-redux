85b01959b32bf07dbb1cc6bb7bea51451175d060
jtrtj <jt.r@live.com>
2018-06-27 15:25:46 -0600

    create new merchant view and route

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -5,9 +5,16 @@ class LittleShopApp < Sinatra::Base
     erb :'merchants/index'
   end
 
+  get '/merchants/new' do
+    erb :'merchants/new'
+  end
+
   get '/merchants/:id' do
     @merchant = Merchant.find(params[:id])
     erb :'merchants/show'
   end
+
+
+
 
 end
