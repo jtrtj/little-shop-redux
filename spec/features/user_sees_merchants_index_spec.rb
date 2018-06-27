e227d37d00ed70beec79d3b1b513cfdf6b1b9b5f
jtrtj <jt.r@live.com>
2018-06-27 17:56:11 -0600

    pass edit button on index spec

--- spec/features/user_sees_merchants_index_spec.rb
+++ spec/features/user_sees_merchants_index_spec.rb
@@ -1,20 +1,25 @@
-describe "User visits index page" do
-    it "should show a list of all merchants" do
-      merchant_1 = Merchant.create(name: "7-eleven")
-      merchant_2 = Merchant.create(name: "Rite Aid")
+describe 'User edits merchant' do
+  it 'should go to edit page when merchant edit button is clicked' do
+    merchant = Merchant.create(name: "7-eleven")
 
-      visit "/merchants"
+    visit '/merchants'
+    click_button "Edit"
 
-      expect(page).to have_content(merchant_1.name)
-      expect(page).to have_content(merchant_2.name)
-    end
-
-    it 'should go to edit page when merchant edit button is clicked' do
-      merchant = Merchant.create(name: "7-eleven")
-
-      visit '/merchants'
-      click_on 'Edit'
-
-      expect(current_path).to eq("/merchants/#{merchant.id}/edit")
-    end
+    expect(current_path).to eq("/merchants/#{merchant.id}/edit")
+  end
 end
+
+describe "User visits index page" do
+  it "should show a list of all merchants" do
+    merchant_1 = Merchant.create(name: "7-eleven")
+    merchant_2 = Merchant.create(name: "Rite Aid")
+
+    visit "/merchants"
+
+    expect(page).to have_content(merchant_1.name)
+    expect(page).to have_content(merchant_2.name)
+  end
+end
+
+
+
