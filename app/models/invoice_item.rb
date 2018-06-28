a121296c73b7d4b81285a481fcd406baeba64c59
Emmie Hayes <emmiehayes2@gmail.com>
2018-06-28 16:05:33 -0600

    add invoice_item model

---
+++ app/models/invoice_item.rb
@@ -0,0 +1,7 @@
+class InvoiceItem < ActiveRecord::Base
+
+  validates_presence_of :item_id,
+                        :invoice_id,
+                        :quantity,
+                        :unit_price
+end
