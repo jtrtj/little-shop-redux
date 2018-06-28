32e93ce020adac5918f0beff39dccd9e3fe62d65
Emmie Hayes <emmiehayes2@gmail.com>
2018-06-27 18:36:48 -0600

    read in csv for merchant table

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+require './data/*.csv'
+require 'CSV'
+
+CSV.foreach('./data/merchants.csv', headers: true, header_converters: :symbol) do |m|
+  Merchant.create(id:         m[:id],
+                  name:       m[:name],
+                  created_at: m[:created_at],
+                  updated_at: m[:updated_at]
+                  )
+end
