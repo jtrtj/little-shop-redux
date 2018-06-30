6847d8425055d7ffb1567dd128b7951b4147600b
Emmie Hayes <emmiehayes2@gmail.com>
2018-06-30 17:51:08 -0600

    build out class methods for item dashboard

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,4 +11,54 @@ RSpec.describe Item do
       expect(item).to_not be_valid
     end
   end
+
+  describe "Class Methods" do
+    it ".average_unit_price" do
+      item_1 = Item.new(
+                        title: 'dog bed',
+                        description: 'a bed for dogs',
+                        price: 700,
+                        image: 'image url'
+                      )
+      item_2 = Item.new(
+                        title: 'cat bed',
+                        description: 'a bed for cats',
+                        price: 500,
+                        image: 'image url'
+                      )
+      expect(@items.average_unit_price).to eq(600)
+    end
+
+    it ".newest" do
+      item_1 = Item.new(
+                        title: 'dog bed',
+                        description: 'a bed for dogs',
+                        price: 700,
+                        image: 'image url'
+                      )
+      item_2 = Item.new(
+                        title: 'cat bed',
+                        description: 'a bed for cats',
+                        price: 500,
+                        image: 'image url'
+                      )
+      expect(@items.newest).to eq(item_2.title)
+    end
+
+    it ".oldest" do
+      item_1 = Item.new(
+                        title: 'dog bed',
+                        description: 'a bed for dogs',
+                        price: 700,
+                        image: 'image url'
+                      )
+      item_2 = Item.new(
+                        title: 'cat bed',
+                        description: 'a bed for cats',
+                        price: 500,
+                        image: 'image url'
+                      )
+      expect(@items.oldest).to eq(item_1.title)
+    end
+  end
 end
