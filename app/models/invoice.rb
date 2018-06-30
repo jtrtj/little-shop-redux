20d2ea898bf520a20250ee46f3427a59964bd1e9
jtrtj <jt.r@live.com>
2018-06-30 17:19:09 -0600

    add test and method for invoice total price

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,4 +4,10 @@ class Invoice < ActiveRecord::Base
                 
   has_many :invoice_items
   belongs_to :merchant
+
+  def total_price
+    invoice_items.map do |invoice_item|
+      invoice_item.quantity * invoice_item.unit_price
+    end.inject(:+)
+  end
 end
