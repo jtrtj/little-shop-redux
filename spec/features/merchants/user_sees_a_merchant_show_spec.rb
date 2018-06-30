b5575f6f5b4498f66fa6d01af22ba268f3cf2ebe
jtrtj <jt.r@live.com>
2018-06-29 20:17:14 -0600

    start cleaning merchant show page

--- spec/features/merchants/user_sees_a_merchant_show_spec.rb
+++ spec/features/merchants/user_sees_a_merchant_show_spec.rb
@@ -1,10 +1,31 @@
-describe "User visits a merchant show page" do
-  it "shows a single merchant" do
-    merchant = Merchant.create(name: "KONG")
+RSpec.describe 'user' do
+  context 'visiting /merchants/id' do
+    it 'should see a single merchant with all of its items' do
+      merchant = Merchant.create(name: "KONG")
 
-    visit "/merchants/#{merchant.id}"
-
-    expect(page).to have_content(merchant.name)
-  end
+      visit "/merchants/#{merchant.id}"
   
+      expect(page).to have_content(merchant.name)
+    end
+
+    it 'can click edit button to edit merchant' do
+      merchant = Merchant.create(name: "KONG")
+
+      visit "/merchants/#{merchant.id}"
+      click_button "Edit"
+
+      expect(current_path).to eq("/merchants/#{merchant.id}/edit")
+    end
+    
+    it 'can click delete button to delete merchant' do
+      merchant = Merchant.create(name: "KONG")
+
+      visit "/merchants/#{merchant.id}"
+      click_button 'Delete'
+
+      expect(current_path).to eq('/merchants')
+      expect(page).to_not have_content(merchant.name)
+    end
+
+  end
 end
