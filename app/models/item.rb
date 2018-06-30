4eda1bbb4b45e50d1171dc0e8c9a23dcdd7eb243
jtrtj <jt.r@live.com>
2018-06-29 20:41:23 -0600

    create merchant has many items relationship and pass test

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,9 @@
 class Item < ActiveRecord::Base
-  has_many :invoice_items
-
   validates_presence_of :title,
                         :description,
                         :price,
                         :merchant_id,
                         :image
+
+  belongs_to :merchant
 end
--- spec/features/merchants/user_sees_a_merchant_show_spec.rb
+++ spec/features/merchants/user_sees_a_merchant_show_spec.rb
@@ -3,9 +3,25 @@ RSpec.describe 'user' do
     it 'should see a single merchant with all of its items' do
       merchant = Merchant.create(name: "KONG")
 
+      item_1 = merchant.items.create(
+                                    title: 'VapeTron',
+                                    description: 'World\'s #1 Vape Pen',
+                                    price: 500,
+                                    image: '/data/image_file_name'
+                                    )
+      item_2 = merchant.items.create(
+                                    title: 'VapeJuice',
+                                    description: 'Grape flavor',
+                                    price: 100,
+                                    image:'/data/image_file_name_'
+                                    )
+
       visit "/merchants/#{merchant.id}"
-  
+
       expect(page).to have_content(merchant.name)
+      expect(page).to have_content(item_1.title)
+      expect(page).to have_content(item_1.price)
+      expect(page).to have_content(item_2.title)
     end
 
     it 'can click edit button to edit merchant' do
