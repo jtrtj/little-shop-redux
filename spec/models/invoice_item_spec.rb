b5aae18e08524fc0eb615aea5d71974f50909d8d
jtrtj <jt.r@live.com>
2018-07-01 17:21:08 -0600

    clean up invoice_items test

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -14,50 +14,23 @@ RSpec.describe InvoiceItem do
 
   describe 'class methods' do
     it '#find_invoice_item_with_highest_unit_price_invoice_id' do
-      invoice_item_1 = InvoiceItem.create(
-                                          item_id: 345,
-                                          invoice_id: 23,
-                                          quantity: 5,
-                                          unit_price: 234567,
-                                        )
-      invoice_item_2 = InvoiceItem.create(
-                                          item_id: 345,
-                                          invoice_id: 44444,
-                                          quantity: 5,
-                                          unit_price: 500,
-                                        )
+      invoice_item_1 = InvoiceItem.create(item_id: 345, invoice_id: 23, quantity: 5, unit_price: 234567)
+      invoice_item_2 = InvoiceItem.create(item_id: 345, invoice_id: 44444, quantity: 10, unit_price: 500)
+      
       expect(InvoiceItem.find_invoice_item_with_highest_unit_price_invoice_id).to eq(invoice_item_1.invoice_id)
     end
 
     it '#find_invoice_item_with_lowest_unit_price_invoice_id' do
-      invoice_item_1 = InvoiceItem.create(
-                                          item_id: 345,
-                                          invoice_id: 23,
-                                          quantity: 5,
-                                          unit_price: 234567,
-                                        )
-      invoice_item_2 = InvoiceItem.create(
-                                          item_id: 345,
-                                          invoice_id: 44444,
-                                          quantity: 5,
-                                          unit_price: 500,
-                                        )
+      invoice_item_1 = InvoiceItem.create(item_id: 345, invoice_id: 23, quantity: 5, unit_price: 234567)
+      invoice_item_2 = InvoiceItem.create(item_id: 345, invoice_id: 44444, quantity: 10, unit_price: 500)
+
       expect(InvoiceItem.find_invoice_item_with_lowest_unit_price_invoice_id).to eq(invoice_item_2.invoice_id)
     end
 
     it '#find_invoice_item_with_highest_quantity_invoice_id' do
-      invoice_item_1 = InvoiceItem.create(
-                                          item_id: 345,
-                                          invoice_id: 23,
-                                          quantity: 5,
-                                          unit_price: 234567,
-                                        )
-      invoice_item_2 = InvoiceItem.create(
-                                          item_id: 345,
-                                          invoice_id: 44444,
-                                          quantity: 10,
-                                          unit_price: 500,
-                                        )
+      invoice_item_1 = InvoiceItem.create(item_id: 345, invoice_id: 23, quantity: 5, unit_price: 234567)
+      invoice_item_2 = InvoiceItem.create(item_id: 345, invoice_id: 44444, quantity: 10, unit_price: 500)
+
       expect(InvoiceItem.find_invoice_item_with_highest_quantity_invoice_id).to eq(invoice_item_2.invoice_id)
     end
 
@@ -69,35 +42,15 @@ RSpec.describe InvoiceItem do
     end
 
     it '#list_by_unit_price_desc' do
-      invoice_item_1 = InvoiceItem.create(
-        item_id: 345,
-        invoice_id: 23,
-        quantity: 5,
-        unit_price: 234567,
-      )
-      invoice_item_2 = InvoiceItem.create(
-        item_id: 345,
-        invoice_id: 44444,
-        quantity: 10,
-        unit_price: 500,
-      )
+      invoice_item_1 = InvoiceItem.create(item_id: 345, invoice_id: 23, quantity: 5, unit_price: 234567)
+      invoice_item_2 = InvoiceItem.create(item_id: 345, invoice_id: 44444, quantity: 10, unit_price: 500)
       
       expect(InvoiceItem.list_by_unit_price_desc).to eq([invoice_item_1, invoice_item_2])
     end
 
     it '#list_by_quantity_desc' do
-      invoice_item_1 = InvoiceItem.create(
-        item_id: 345,
-        invoice_id: 23,
-        quantity: 5,
-        unit_price: 234567,
-      )
-      invoice_item_2 = InvoiceItem.create(
-        item_id: 345,
-        invoice_id: 44444,
-        quantity: 10,
-        unit_price: 500,
-      )
+      invoice_item_1 = InvoiceItem.create(item_id: 345, invoice_id: 23, quantity: 5, unit_price: 234567)
+      invoice_item_2 = InvoiceItem.create(item_id: 345, invoice_id: 44444, quantity: 10, unit_price: 500)
       
       expect(InvoiceItem.list_by_quantity_desc).to eq([invoice_item_2, invoice_item_1])
     end
