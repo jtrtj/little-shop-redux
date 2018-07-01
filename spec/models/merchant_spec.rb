13037fd1393b9887b1554f804b4f3a45f18024c1
Emmie Hayes <emmiehayes2@gmail.com>
2018-06-30 18:33:54 -0600

    shell for class and instance methods for merchant dashboard

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -6,5 +6,32 @@ RSpec.describe Merchant do
       expect(merchant).to_not be_valid
     end
   end
-  
+
+  describe "Instance Methods" do
+    it ".total_merchant_items" do
+
+    end
+
+    it ".average_item_price" do
+
+    end
+
+    it ".total_price_all_items" do
+
+    end
+  end
+
+  describe "Class Methods" do
+    it ".most_items" do
+
+    end
+
+    it ".highest_priced_item" do
+
+    end
+
+    it ".total_price_all_items" do
+
+    end
+  end
 end
