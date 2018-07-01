63d9fbcd7c4a272ddcfd57b75487c3ccf0b4fe09
Eliot Swank <eliotswank@gmail.com>
2018-07-01 17:26:14 -0600

    adds test and functionality for merchant dashboard instances

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -1,5 +1,6 @@
 class LittleShopApp < Sinatra::Base
   get '/merchants/dashboard' do
+    @merchants = Merchant.all
     @most_items = Merchant.most_items
     @highest_priced_item = Merchant.highest_priced_item
     erb :'merchants/dashboard'
