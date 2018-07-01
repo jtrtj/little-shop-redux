c5904f65289c09eec14d566d1bfafff43f5f7189
jtrtj <jt.r@live.com>
2018-07-01 17:16:00 -0600

    find_invoice_item_with_highest_quantity_invoice_id test passes

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -15,6 +15,10 @@ class InvoiceItem < ActiveRecord::Base
     list_by_unit_price_desc.last.invoice_id
   end
 
+  def self.find_invoice_item_with_highest_quantity_invoice_id
+    list_by_quantity_desc.first.invoice_id
+  end
+
   def self.list_by_unit_price_desc
     order(unit_price: :desc)
   end
