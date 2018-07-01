3669c5e5a07fe87163243ad7dfe46088800a94e5
Emmie Hayes <emmiehayes2@gmail.com>
2018-06-30 18:22:55 -0600

    add all class methods for item dashboard with passing test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe Item do
   describe "Validations" do
     it "should have attributes" do
-      item = Item.new(
+      item = Item.create(
                       title: nil,
                       description: nil,
                       price: nil,
@@ -13,52 +13,76 @@ RSpec.describe Item do
   end
 
   describe "Class Methods" do
-    it ".average_unit_price" do
-      item_1 = Item.new(
+    it '.total_item_count' do
+      item_1 = Item.create(
                         title: 'dog bed',
                         description: 'a bed for dogs',
                         price: 700,
-                        image: 'image url'
+                        image: 'image url',
+                        merchant_id: 3456
                       )
-      item_2 = Item.new(
+      item_2 = Item.create(
                         title: 'cat bed',
                         description: 'a bed for cats',
                         price: 500,
-                        image: 'image url'
+                        image: 'image url',
+                        merchant_id: 3456
                       )
-      expect(@items.average_unit_price).to eq(600)
+      expect(Item.total_item_count).to eq(2)
     end
 
-    it ".newest" do
-      item_1 = Item.new(
+    it '.average_unit_price' do
+      item_1 = Item.create(
                         title: 'dog bed',
                         description: 'a bed for dogs',
                         price: 700,
-                        image: 'image url'
+                        image: 'image url',
+                        merchant_id: 3456
                       )
-      item_2 = Item.new(
+      item_2 = Item.create(
                         title: 'cat bed',
                         description: 'a bed for cats',
                         price: 500,
-                        image: 'image url'
+                        image: 'image url',
+                        merchant_id: 3456
                       )
-      expect(@items.newest).to eq(item_2.title)
+      expect(Item.average_unit_price).to eq(600)
     end
 
-    it ".oldest" do
-      item_1 = Item.new(
+    it '.newest' do
+      item_1 = Item.create(
                         title: 'dog bed',
                         description: 'a bed for dogs',
                         price: 700,
-                        image: 'image url'
+                        image: 'image url',
+                        merchant_id: 3456
                       )
-      item_2 = Item.new(
+      item_2 = Item.create(
                         title: 'cat bed',
                         description: 'a bed for cats',
                         price: 500,
-                        image: 'image url'
+                        image: 'image url',
+                        merchant_id: 3456
                       )
-      expect(@items.oldest).to eq(item_1.title)
+      expect(Item.newest).to eq(item_2.title)
+    end
+
+    it '.oldest' do
+      item_1 = Item.create(
+                        title: 'dog bed',
+                        description: 'a bed for dogs',
+                        price: 700,
+                        image: 'image url',
+                        merchant_id: 3456
+                      )
+      item_2 = Item.create(
+                        title: 'cat bed',
+                        description: 'a bed for cats',
+                        price: 500,
+                        image: 'image url',
+                        merchant_id: 3456
+                      )
+      expect(Item.oldest).to eq(item_1.title)
     end
   end
 end
