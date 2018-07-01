10c5e1da8f023820658e85dbed2d024d858618e5
Eliot Swank <eliotswank@gmail.com>
2018-07-01 16:11:26 -0600

    builds test for merchant dash most_items

--- spec/features/merchants/user_sees_a_merchant_dashboard_spec.rb
+++ spec/features/merchants/user_sees_a_merchant_dashboard_spec.rb
@@ -5,5 +5,36 @@ RSpec.describe "User" do
 
       expect(page).to have_content("Merchants Dashboard")
     end
+    it "should see Merchant With Most Items" do
+      merchant_1 = Merchant.create(name: "Dogs4Life")
+      merchant_2 = Merchant.create(name: "Cats, wow")
+
+      item_1 = Item.create(
+                          title: 'dog bed',
+                          description: 'a bed for dogs',
+                          price: 700,
+                          image: 'image url',
+                          merchant_id: merchant_1.id
+                          )
+      item_2 = Item.create(
+                          title: 'cat bed',
+                          description: 'a bed for cats',
+                          price: 500,
+                          image: 'image url',
+                          merchant_id: merchant_2.id
+                          )
+      item_3= Item.create(
+                          title: 'dog bone',
+                          description: 'a bone for dogs',
+                          price: 20,
+                          image: 'image url',
+                          merchant_id: merchant_1.id
+                          )
+
+      visit 'merchants/dashboard'
+
+      
+      expect(page).to have_content("Merchant With Most Items: #{merchant.most_items.name}")
+    end
   end
 end
