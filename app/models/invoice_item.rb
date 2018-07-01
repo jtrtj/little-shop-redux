26bb660cce62252dea1bffc6151636639d43a2e5
jtrtj <jt.r@live.com>
2018-07-01 16:52:31 -0600

    tests green in invoice_item

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -10,4 +10,8 @@ class InvoiceItem < ActiveRecord::Base
   def self.find_invoice_item_with_highest_unit_price_invoice_id
     order(unit_price: :desc).first.invoice_id
   end
+
+  def self.find_invoice_item_with_lowest_unit_price_invoice_id
+    order(unit_price: :desc).last.invoice_id
+  end
 end
