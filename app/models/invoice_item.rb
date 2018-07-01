2c8c1757710392705d4ada1836a660b1cd470611
jtrtj <jt.r@live.com>
2018-07-01 17:19:17 -0600

    all tests green on invoice_item

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -19,6 +19,10 @@ class InvoiceItem < ActiveRecord::Base
     list_by_quantity_desc.first.invoice_id
   end
 
+  def self.find_invoice_item_with_lowest_quantity_invoice_id
+    list_by_quantity_desc.last.invoice_id
+  end
+
   def self.list_by_unit_price_desc
     order(unit_price: :desc)
   end
