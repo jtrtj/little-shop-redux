f2d93cc155bcd2d07b0e99ddd44df7cdb5f5d06d
jtrtj <jt.r@live.com>
2018-07-01 16:53:55 -0600

    refactor invoice_items

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -8,10 +8,14 @@ class InvoiceItem < ActiveRecord::Base
   belongs_to :item
 
   def self.find_invoice_item_with_highest_unit_price_invoice_id
-    order(unit_price: :desc).first.invoice_id
+    list_by_unit_price_desc.first.invoice_id
   end
-
+  
   def self.find_invoice_item_with_lowest_unit_price_invoice_id
-    order(unit_price: :desc).last.invoice_id
+    list_by_unit_price_desc.last.invoice_id
+  end
+  
+  def self.list_by_unit_price_desc
+    order(unit_price: :desc)
   end
 end
