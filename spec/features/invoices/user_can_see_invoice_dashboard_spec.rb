1c99e9f70c0abc090d029d8722619588e3c4dcb9
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-02 20:00:05 -0600

    create test for percentage by status invoice dashboard

--- spec/features/invoices/user_can_see_invoice_dashboard_spec.rb
+++ spec/features/invoices/user_can_see_invoice_dashboard_spec.rb
@@ -4,5 +4,20 @@ RSpec.describe 'User' do
       visit '/invoices/dashboard'
       expect(page).to have_content('Invoices Dashboard')
     end
+
+    it 'should see percentage of each invoice status' do
+      Invoice.create(merchant_id: 99999, status: 'shipped')
+      Invoice.create(merchant_id: 99999, status: 'shipped')
+      Invoice.create(merchant_id: 99999, status: 'pending')
+      Invoice.create(merchant_id: 99999, status: 'pending')
+      Invoice.create(merchant_id: 99999, status: 'pending')
+      Invoice.create(merchant_id: 99999, status: 'returned')
+
+      visit '/invoices/dashboard'
+
+      expect(page).to have_content("Shipped: #{@invoice.percentage_by_status(shipped)}")
+      expect(page).to have_content("Pending: #{@invoice.percentage_by_status(pending)}")
+      expect(page).to have_content("Returned: #{@invoice.percentage_by_status(returned)}")
+    end
   end
 end
