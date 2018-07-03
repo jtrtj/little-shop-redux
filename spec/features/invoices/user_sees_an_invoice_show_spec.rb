43a92dfd93e87b92fff9333caa7b180b938da8f3
Eliot Swank <eliotswank@gmail.com>
2018-07-02 18:36:21 -0600

    adds test for invoice show--merchant name

--- spec/features/invoices/user_sees_an_invoice_show_spec.rb
+++ spec/features/invoices/user_sees_an_invoice_show_spec.rb
@@ -36,5 +36,43 @@ RSpec.describe "Invoice Show" do
 
       expect(page).to have_content("Invoice: #{invoice_1.id} - #{invoice_1.status}")
     end
+
+    it "should see the merchant name" do
+      merchant = Merchant.create(id: 99999, name: "Vapin' the Day Away")
+      invoice_1 = Invoice.create(merchant_id: 99999, status: 'shipped')
+      invoice_2 = Invoice.create(merchant_id: 77777, status: 'shipped')
+      item_1 = Item.create(
+                          title: 'VapeTron',
+                          description: 'World\'s #1 Vape Pen',
+                          price: 500,
+                          image: '/data/image_file_name',
+                          merchant_id: 6789
+                         )
+
+      invoice_item_1 = InvoiceItem.create(
+                                          item_id: item_1.id,
+                                          invoice_id: invoice_1.id,
+                                          quantity: 5,
+                                          unit_price: item_1.price,
+                                         )
+      item_2 = Item.create(
+                          title: 'VapeWnd',
+                          description: 'World\'s #1 Vape Pen',
+                          price: 1000,
+                          image: '/data/image_file_name',
+                          merchant_id: 6789
+                         )
+
+      invoice_item_2 = InvoiceItem.create(
+                                          item_id: item_2.id,
+                                          invoice_id: invoice_2.id,
+                                          quantity: 5,
+                                          unit_price: item_2.price,
+                                         )
+
+      visit "/invoices/#{invoice_1.id}"
+
+      expect(page).to have_content(invoice_1.merchant.name)
+    end
   end
 end
