bcd5dd2e93f51c45ad83a96914286fe93aaf05d1
jtrtj <jt.r@live.com>
2018-07-02 20:38:44 -0600

    test is green

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,8 +4,9 @@ class Invoice < ActiveRecord::Base
                 
   has_many :invoice_items
   belongs_to :merchant
-  
+
   def total_price
+    invoice_items.sum('unit_price * quantity')
   end
 
   def self.amount_by_status(status)
