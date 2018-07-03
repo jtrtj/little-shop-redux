88f7a3e769dc025c3c5c82754821df066fa56f96
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-02 21:20:50 -0600

    feature test pass for item dashboard

--- spec/features/invoices/user_can_see_invoice_dashboard_spec.rb
+++ spec/features/invoices/user_can_see_invoice_dashboard_spec.rb
@@ -1,11 +1,71 @@
 RSpec.describe 'User' do
   context 'visiting /invoices/dashboard' do
     it 'should see a header' do
+      invoice_1 = Invoice.create(merchant_id: 99999, status: 'shipped')
+      invoice_2 = Invoice.create(merchant_id: 77777, status: 'shipped')
+      item_1 = Item.create(
+                          title: 'VapeTron',
+                          description: 'World\'s #1 Vape Pen',
+                          price: 500,
+                          image: '/data/image_file_name',
+                          merchant_id: 6789
+                         )
+
+      invoice_item_1 = InvoiceItem.create(
+                                          item_id: item_1.id,
+                                          invoice_id: invoice_1.id,
+                                          quantity: 5,
+                                          unit_price: item_1.price,
+                                         )
+      item_2 = Item.create(
+                          title: 'VapeWnd',
+                          description: 'World\'s #1 Vape Pen',
+                          price: 1000,
+                          image: '/data/image_file_name',
+                          merchant_id: 6789
+                         )
+
+      invoice_item_2 = InvoiceItem.create(
+                                          item_id: item_2.id,
+                                          invoice_id: invoice_2.id,
+                                          quantity: 5,
+                                          unit_price: item_2.price,
+                                         )
       visit '/invoices/dashboard'
       expect(page).to have_content('Invoices Dashboard')
     end
 
     it 'should see percentage of each invoice status' do
+      invoice_1 = Invoice.create(merchant_id: 99999, status: 'shipped')
+      invoice_2 = Invoice.create(merchant_id: 77777, status: 'shipped')
+      item_1 = Item.create(
+                          title: 'VapeTron',
+                          description: 'World\'s #1 Vape Pen',
+                          price: 500,
+                          image: '/data/image_file_name',
+                          merchant_id: 6789
+                         )
+
+      invoice_item_1 = InvoiceItem.create(
+                                          item_id: item_1.id,
+                                          invoice_id: invoice_1.id,
+                                          quantity: 5,
+                                          unit_price: item_1.price,
+                                         )
+      item_2 = Item.create(
+                          title: 'VapeWnd',
+                          description: 'World\'s #1 Vape Pen',
+                          price: 1000,
+                          image: '/data/image_file_name',
+                          merchant_id: 6789
+                         )
+
+      invoice_item_2 = InvoiceItem.create(
+                                          item_id: item_2.id,
+                                          invoice_id: invoice_2.id,
+                                          quantity: 5,
+                                          unit_price: item_2.price,
+                                         )
       Invoice.create(merchant_id: 99999, status: 'shipped')
       Invoice.create(merchant_id: 99999, status: 'shipped')
       Invoice.create(merchant_id: 99999, status: 'pending')
@@ -14,6 +74,7 @@ RSpec.describe 'User' do
       Invoice.create(merchant_id: 99999, status: 'returned')
 
       visit '/invoices/dashboard'
+
 
       expect(page).to have_content("Shipped: #{Invoice.percentage_by_status('shipped')}")
       expect(page).to have_content("Pending: #{Invoice.percentage_by_status('pending')}")
@@ -52,9 +113,8 @@ RSpec.describe 'User' do
                                           unit_price: item_2.price,
                                          )
       visit '/invoices/dashboard'
-
-      expect(page).to have_content("Highest Invoice Total: #{Invoice.invoice_with_lowest_total.id}")
-      expect(page).to have_content("Lowest Invoice Total: #{Invoice.invoice_with_highest_total.id}")
+      expect(page).to have_content("Highest Invoice Total: #{invoice_2.id}")
+      expect(page).to have_content("Lowest Invoice Total: #{invoice_1.id}")
     end
   end
 end
