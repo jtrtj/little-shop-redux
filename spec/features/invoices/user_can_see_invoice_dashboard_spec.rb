79e98a2695d75232261b70c275dbe0fcc679e150
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-02 20:40:00 -0600

    wip feature testing invoice dash

--- spec/features/invoices/user_can_see_invoice_dashboard_spec.rb
+++ spec/features/invoices/user_can_see_invoice_dashboard_spec.rb
@@ -19,5 +19,42 @@ RSpec.describe 'User' do
       expect(page).to have_content("Pending: #{Invoice.percentage_by_status('pending')}")
       expect(page).to have_content("Returned: #{Invoice.percentage_by_status('returned')}")
     end
+
+    it 'should see highest and lowest total invoice prices' do
+      invoice_1 = Invoice.create(merchant_id: 99999, status: 'shipped')
+      invoice_2 = Invoice.create(merchant_id: 77777, status: 'shipped')
+      item_1 = Item.create(
+                          title: 'VapeTron',
+                          description: 'World\'s #1 Vape Pen',
+                          price: 500,
+                          image: '/data/image_file_name',
+                          merchant_id: 6789
+                         )
+
+      invoice_item_1 = InvoiceItem.create(
+                                          item_id: item_1.id,
+                                          invoice_id: invoice_1.id,
+                                          quantity: 5,
+                                          unit_price: item_1.price,
+                                         )
+      item_2 = Item.create(
+                          title: 'VapeWnd',
+                          description: 'World\'s #1 Vape Pen',
+                          price: 1000,
+                          image: '/data/image_file_name',
+                          merchant_id: 6789
+                         )
+
+      invoice_item_2 = InvoiceItem.create(
+                                          item_id: item_2.id,
+                                          invoice_id: invoice_2.id,
+                                          quantity: 5,
+                                          unit_price: item_2.price,
+                                         )
+      visit '/invoices/dashboard'
+
+      expect(page).to have_content("Highest Invoice Total: #{Invoice.invoice_with_lowest_total.id}")
+      expect(page).to have_content("Lowest Invoice Total: #{Invoice.invoice_with_highest_total.id}")
+    end
   end
 end
